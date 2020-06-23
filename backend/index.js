c93e37fc7f8cc8f6ba60dae3e6029e3a11a9a84d
NewbieTao <39129447+taozeroxii@users.noreply.github.com>
2020-06-23 22:21:43 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const server = express();
+const session = require('express-session')
+const bodyParser = require("body-parser");
+const { check, validationResult } = require('express-validator');
 const PORT = 3000;
 
-server.get('*',(req,res)=>{ res.json({ message:'server is start'})});
+//ตั้งค่า session
+server.use(session({
+  secret: 'CpaWebsitemodifytest',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {  }
+}))
+
+//ตั้งค่าการ parse ตัวแปร client ที่ส่งข้อมูลเข้ามา
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+ 
+server.post('/',(req,res)=>res.json(req.body))
+
+server.get('*',(req,res)=>{ res.end(`server is start`)});
 
 server.listen(PORT,() => console.log(`this server running on PORT : ${PORT}`))
 
